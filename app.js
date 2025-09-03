0197f0ab9b8021e60f967ebecd9c255b9c1b488b
Matheus Ferraz <matheusaugustosabinoferraz@outlook.com>
2025-09-03 14:57:04 -0300

    Atualizações de layout atualizações do css em geral responsividade atualização do javascript Seção skills atualizadas com cards Realizado icones com link para contatos seção projetos atualizada

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
 let imagemdinamica = document.getElementById ("imagemdinamica")  //variavel do elemento html
 let imagens = [
     "imagem1.jpg",
-    "imagem2.jpg",    // variavel das imagens 
+    "imagem2.jpg",    // ARRAY
     "imagem3.jpg"
 ];
 let imagematual = 0; // começa na imagem 1 , variavel da imagem atual 
 function trocaimagem() {
     imagematual = (imagematual + 1) % imagens.length;
     imagemdinamica.src = imagens[imagematual] 
-    setInterval (trocaimagem , 4000);
-    
+    setInterval (trocaimagem , 4000);   
 }
-function temapurple() {
-    let botao = document.getElementById ("botaoTema")
-    let body = document.body;
-    body.classList.toggle ("tema-purple")
-    if (body.classList.contains("tema-purple" )) {
-        botao.innerText = "Modo Classico";
-    } else {
-        botao.innerText = "Modo Nocturnal Cinzel"
-    }
+ //chama a função
+function temaPurple() {
+  const botao = document.getElementById("botaoTema");
+  const body = document.body;
+
+  body.classList.toggle("tema-purple");
+
+  if (body.classList.contains("tema-purple")) {
+    botao.innerText = "Tema Clássico";
+    localStorage.setItem("tema", "escuro");
+  } else {
+    botao.innerText = "Tema Escuro";
+    localStorage.setItem("tema", "claro");
+  }
 }
+window.addEventListener("DOMContentLoaded", () => {
+  const botao = document.getElementById("botaoTema");
+  const body = document.body;
+  const temaSalvo = localStorage.getItem("tema");
+
+  if (temaSalvo === "escuro") {
+    body.classList.add("tema-purple");
+    botao.innerText = "Tema Clássico";
+  } else {
+    botao.innerText = "Tema Escuro";
+  }
+});
